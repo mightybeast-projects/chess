4b2aae5db0f2f51fbadaaff5a2413646911aea28
MightyBeast <zgpoter@gmail.com>
2023-07-05 13:41:04 +0300

    -chore- : Added more tests for pawns en passant.

--- Tests/Pieces/PawnTests/EnPassantTests/BlackPawnEnPassantTests.cs
+++ Tests/Pieces/PawnTests/EnPassantTests/BlackPawnEnPassantTests.cs
@@ -8,16 +8,14 @@ namespace Chess.Tests.Pieces.PawnTests.EnPassantTests;
 [TestFixture]
 internal class BlackPawnEnPassantTests : BoardTestFixtureSetUp
 {
+    private Pawn blackPawn;
+    private Pawn f2whitePawn;
+    private Pawn h2whitePawn;
+
     [Test]
     public void BlackPawn_HasEnPassantLegalMove()
     {
-        Pawn blackPawn = new Pawn(board.GetTile("g4"), Color.BLACK);
-        Pawn f2whitePawn = new Pawn(board.GetTile("f2"), Color.WHITE);
-        Pawn h2whitePawn = new Pawn(board.GetTile("h2"), Color.WHITE);
-
-        board.AddPiece(blackPawn);
-        board.AddPiece(f2whitePawn);
-        board.AddPiece(h2whitePawn);
+        AddPawns();
 
         f2whitePawn.Move("f4");
 
@@ -33,4 +31,60 @@ internal class BlackPawnEnPassantTests : BoardTestFixtureSetUp
             board.GetTile("h3")
         }));
     }
+
+    [Test]
+    public void BlackPawn_DoesNotHaveEnPassantLegalMove_IfLastMovedPieceIsNotPawn()
+    {
+        AddPawns();
+
+        Knight whiteKnight = new Knight(board.GetTile("e2"), Color.WHITE);
+
+        board.AddPiece(whiteKnight);
+
+        f2whitePawn.Move("f4");
+        whiteKnight.Move("d4");
+
+        AssertLegalMovesDoesNotHaveEnPassant();
+    }
+
+    [Test]
+    public void BlackPawn_DoesNotHaveEnPassantLegalMove_IfPieceIsNotPawn()
+    {
+        AddPawns();
+
+        Knight whiteKnight = new Knight(board.GetTile("e2"), Color.WHITE);
+
+        board.AddPiece(whiteKnight);
+
+        whiteKnight.Move("f4");
+
+        AssertLegalMovesDoesNotHaveEnPassant();
+    }
+
+    [Test]
+    public void WhitePawn_DoesNotHaveEnPassantLegalMove_IfEnemyPawnDidNotMoveTwoTilesAtATime()
+    {
+        AddPawns();
+
+        f2whitePawn.Move("f3");
+        f2whitePawn.Move("f4");
+
+        AssertLegalMovesDoesNotHaveEnPassant();
+    }
+
+    private void AddPawns()
+    {
+        blackPawn = new Pawn(board.GetTile("g4"), Color.BLACK);
+        f2whitePawn = new Pawn(board.GetTile("f2"), Color.WHITE);
+        h2whitePawn = new Pawn(board.GetTile("h2"), Color.WHITE);
+
+        board.AddPiece(blackPawn);
+        board.AddPiece(f2whitePawn);
+        board.AddPiece(h2whitePawn);
+    }
+
+    private void AssertLegalMovesDoesNotHaveEnPassant() =>
+        Assert.That(blackPawn.legalMoves, Is.EquivalentTo(new Tile[] {
+            board.GetTile("g3")
+        }));
 }
--- Tests/Pieces/PawnTests/EnPassantTests/WhitePawnEnPassantTests.cs
+++ Tests/Pieces/PawnTests/EnPassantTests/WhitePawnEnPassantTests.cs
@@ -8,16 +8,15 @@ namespace Chess.Tests.Pieces.PawnTests.EnPassantTests;
 [TestFixture]
 internal class WhitePawnEnPassantTests : BoardTestFixtureSetUp
 {
+    private Pawn whitePawn;
+    private Pawn a7blackPawn;
+    private Pawn c7blackPawn;
+
+
     [Test]
     public void WhitePawn_HasEnPassantLegalMove()
     {
-        Pawn whitePawn = new Pawn(board.GetTile("b5"), Color.WHITE);
-        Pawn a7blackPawn = new Pawn(board.GetTile("a7"), Color.BLACK);
-        Pawn c7blackPawn = new Pawn(board.GetTile("c7"), Color.BLACK);
-
-        board.AddPiece(whitePawn);
-        board.AddPiece(a7blackPawn);
-        board.AddPiece(c7blackPawn);
+        AddPawns();
 
         a7blackPawn.Move("a5");
 
@@ -33,4 +32,60 @@ internal class WhitePawnEnPassantTests : BoardTestFixtureSetUp
             board.GetTile("c6")
         }));
     }
+
+    [Test]
+    public void WhitePawn_DoesNotHaveEnPassantLegalMove_IfLastMovedPieceIsNotPawn()
+    {
+        AddPawns();
+
+        Knight blackKnight = new Knight(board.GetTile("d7"), Color.BLACK);
+
+        board.AddPiece(blackKnight);
+
+        c7blackPawn.Move("c5");
+        blackKnight.Move("e5");
+
+        AssertLegalMovesDoesNotHaveEnPassant();
+    }
+
+    [Test]
+    public void WhitePawn_DoesNotHaveEnPassantLegalMove_IfPieceIsNotPawn()
+    {
+        AddPawns();
+
+        Knight blackKnight = new Knight(board.GetTile("d7"), Color.BLACK);
+
+        board.AddPiece(blackKnight);
+
+        blackKnight.Move("c5");
+
+        AssertLegalMovesDoesNotHaveEnPassant();
+    }
+
+    [Test]
+    public void WhitePawn_DoesNotHaveEnPassantLegalMove_IfEnemyPawnDidNotMoveTwoTilesAtATime()
+    {
+        AddPawns();
+
+        c7blackPawn.Move("c6");
+        c7blackPawn.Move("c5");
+
+        AssertLegalMovesDoesNotHaveEnPassant();
+    }
+
+    private void AddPawns()
+    {
+        whitePawn = new Pawn(board.GetTile("b5"), Color.WHITE);
+        a7blackPawn = new Pawn(board.GetTile("a7"), Color.BLACK);
+        c7blackPawn = new Pawn(board.GetTile("c7"), Color.BLACK);
+
+        board.AddPiece(whitePawn);
+        board.AddPiece(a7blackPawn);
+        board.AddPiece(c7blackPawn);
+    }
+
+    private void AssertLegalMovesDoesNotHaveEnPassant() =>
+        Assert.That(whitePawn.legalMoves, Is.EquivalentTo(new Tile[] {
+            board.GetTile("b6")
+        }));
 }
